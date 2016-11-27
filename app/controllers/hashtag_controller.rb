79f0e7d2d1ae6c8a3a4c847bd95c92e3bd963781
Nikolas Yanez <yaneznikolas@gmail.com>
2016-11-27 18:26:29 -0500

    fixed search

--- app/controllers/hashtag_controller.rb
+++ app/controllers/hashtag_controller.rb
@@ -4,8 +4,18 @@ class HashtagController < ApplicationController
 
   def index
     unless params[:q].nil?
-      @hashtags = $client.search(params[:q]).take(2)
+      hashtag_collection = {}
+      @results = $client.search(params[:q], until: DateTime.yesterday.strftime('%F')).map do |tweets|
+        tweets.hashtags.map do |hashtag|
+          hashtag_name = hashtag.text.downcase
+          if hashtag_collection.keys.include?(hashtag_name)
+            hashtag_collection[hashtag_name] += 1;
+          else
+            hashtag_collection[hashtag_name] = 1;
+          end
+        end
+      end
+      hashtag_collection.sort_by{|key, value| value}.reverse
     end
   end
-
 end

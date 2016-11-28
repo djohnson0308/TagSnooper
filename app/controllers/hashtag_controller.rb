49a729461b219bdfbde0692cc2f0dcf5a5515ac5
Nikolas Yanez <yaneznikolas@gmail.com>
2016-11-28 11:45:15 -0500

    successfully pulling hashtags

--- app/controllers/hashtag_controller.rb
+++ app/controllers/hashtag_controller.rb
@@ -3,19 +3,7 @@ require 'twitter_search'
 class HashtagController < ApplicationController
 
   def index
-    unless params[:q].nil?
-      hashtag_collection = {}
-      @results = $client.search(params[:q], until: DateTime.yesterday.strftime('%F')).map do |tweets|
-        tweets.hashtags.map do |hashtag|
-          hashtag_name = hashtag.text.downcase
-          if hashtag_collection.keys.include?(hashtag_name)
-            hashtag_collection[hashtag_name] += 1;
-          else
-            hashtag_collection[hashtag_name] = 1;
-          end
-        end
-      end
-      hashtag_collection.sort_by{|key, value| value}.reverse
-    end
+    @results = Hashtag.tweet_search(params[:q])
   end
+
 end
--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,3 +1,19 @@
 class Hashtag < ApplicationRecord
   belongs_to :user
+
+  def self.tweet_search(term)
+    hashtag_collection = {}
+    $client.search(term, until: DateTime.yesterday.strftime('%F')).map do |tweets|
+      tweets.hashtags.map do |hashtag|
+        hashtag_name = hashtag.text.downcase
+        if hashtag_collection.keys.include?(hashtag_name)
+          hashtag_collection[hashtag_name] += 1;
+        else
+          hashtag_collection[hashtag_name] = 1;
+        end
+      end
+    end
+    hashtag_collection.sort_by{|key, value| value}.reverse
+  end
+
 end

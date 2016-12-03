dc18ca5f39959b3fe0e341e95c1a205604ba4d68
Nikolas Yanez <yaneznikolas@gmail.com>
2016-12-03 16:08:25 -0500

    added delete topics button

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -8,12 +8,17 @@ $(document).on('turbolinks:load', function(){
     var optionText = $('#topic_search option:selected').text();
     var url = '/dashboard?topic_name=' + optionText;
     window.location = url
-    // $.get({
-    //  url: '/topic/hashtags',
-    //  data: {topicName: optionText}
-  //  });
   });
 });
+
+function deleteTopic(){
+  var deleteTopicName = $('#topic_search option:selected').text();
+  var url = '/topics/' + deleteTopicName;
+  $.ajax({
+    url: url,
+    type: 'DELETE'
+  });
+}
 
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -20,8 +20,19 @@ class TopicsController < ApplicationController
     redirect_to dashboard_path(topic_name: params[:topicName])
   end
 
+  def destroy
+    Topic.find_by(name: delete_topic_params).destroy
+    respond_to do |format|
+      format.html { redirect_to dashboard_path, notice: 'Topic was successfully deleted.' }
+    end
+  end
+
   private
   def topic_params
     params.require(:topic).permit(:name)
+  end
+
+  def delete_topic_params
+    params.require(:name)
   end
 end
